47dea2117aa84e7233212e6de9801fb44e9be1c0
Ghostmailer1 <74950510+Ghostmailer1@users.noreply.github.com>
2021-03-04 20:32:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ class script(object):
 
 
     START_MSG = """ <b>Hi {}
-Sorry.. This Bot Can Only Be Used in Movie Club Group!
+Sorry.. I'm made only for TELEMAX by My</b> <a href='https://t.me/Sunisk'>Creator</a> ! Check About to know more
 
 """
 
@@ -21,17 +21,17 @@ No need add each filter again!
 Bot will automatically search for your file and give links to that!
 </i>
 
-<b>© @MOVIECLUB_CHAT</b>"""
+<b>© @Sunisk</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Dulquer◢ ◤</b>
+    ABOUT_MSG = """⭕️<b>My Name : Media Helper◢ ◤</b>
     
-⭕️<b>Creater :</b> @NickxFury
+⭕️<b>Creater :</b> @Sunisk
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Join Our Channels :</b> <a href='https://t.me/MOVIECLUB_LINKS'>Click Here</a> 
+⭕️<b>Join Our Channels :</b> <a href='https://t.me/telemaxchanel'>Click Here</a> 
 
 """
